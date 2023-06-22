67a4abab1f95488926e69f3ae68bbf833d575367
ilq1 <andrey_kalashnikov_am@list.ru>
2023-06-23 01:06:44 +0700

    commit

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -33,7 +33,15 @@ public class MainActivity2 extends AppCompatActivity {
         accessoryDatabase db = Room.databaseBuilder(getApplicationContext(), accessoryDatabase.class, "Accessory-db").allowMainThreadQueries().build();
 
         accessoryList = new ArrayList<>();
-        accessoryList.addAll(db.accessoryDao().getAccessoryByPrice(price));
+        accessoryList = new ArrayList<>();
+        accessoryList.addAll(db.accessoryDao().getAccessoryByTypeAndPrice("processor", price));
+        accessoryList.addAll(db.accessoryDao().getAccessoryByTypeAndPrice("video_card", price));
+        accessoryList.addAll(db.accessoryDao().getAccessoryByTypeAndPrice("ram", price));
+        accessoryList.addAll(db.accessoryDao().getAccessoryByTypeAndPrice("motherboard", price));
+        accessoryList.addAll(db.accessoryDao().getAccessoryByTypeAndPrice("frame", price));
+        accessoryList.addAll(db.accessoryDao().getAccessoryByTypeAndPrice("disk", price));
+        accessoryList.addAll(db.accessoryDao().getAccessoryByTypeAndPrice("cooler", price));
+        accessoryList.addAll(db.accessoryDao().getAccessoryByTypeAndPrice("power_unit", price));
         ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, new ArrayList<>());
         ArrayList<String> displayAccessories = new ArrayList<>();
         for (Accessory item : accessoryList) {
